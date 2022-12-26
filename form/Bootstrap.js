d0427fe909cc7291637c7d260a1790f5c3c33ce8
Ahmad Yasi Faizi <yasifaizi10@gmail.com>
2022-12-26 21:46:16 +0700

    bootstrap

---
+++ form/Bootstrap.js
@@ -0,0 +1,17 @@
+[
+    {
+        "name": "Leo",
+        "email": "Leo@Gmail.com",
+        "phone": "123-432-3421"
+    },
+    {
+        "name": "Micheal",
+        "email": "Micheal@racoon.com",
+        "phone": "123-432-4561"
+    },
+    {
+        "name": "Donatello",
+        "email": "Donatello@Gmail.com",
+        "phone": "123-666-2341"
+    }
+]
